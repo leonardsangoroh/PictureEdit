8e089d147c2e07a57612bebe5fd90ce5009a04dc
Leonard Sangoroh <61079370+leonardsangoroh@users.noreply.github.com>
2024-02-05 23:53:26 +0300

    feat: enable sepia tone edit functionality

--- PictureEdit/ViewController.swift
+++ PictureEdit/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import CoreImage
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
 
@@ -13,6 +14,10 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate & UINavi
     @IBOutlet var intensity: UISlider!
     
     var currentImage: UIImage!
+    ///CoreImage component that handles rendering
+    var context: CIContext!
+    ///store filter the user activates
+    var currentFilter: CIFilter!
     
     
     override func viewDidLoad() {
@@ -21,6 +26,9 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate & UINavi
         
         title = "YACIFP"
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .camera, target: self, action: #selector(importPicture))
+        
+        context = CIContext()
+        currentFilter = CIFilter(name: "CISepiaTone")
     }
     
     @objc func importPicture(){
@@ -36,12 +44,34 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate & UINavi
         dismiss(animated: true)
         
         currentImage = image
+        /// CIImage is the Core Image equivalent of UIImage
+        /// sent the result into the current Core Image Filter using KCIInputImageKey
+        let beginImage = CIImage(image: currentImage)
+        currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+        applyProcessing()
     }
 
     @IBAction func changeFilter(_ sender: Any) {
     }
     
     @IBAction func save(_ sender: Any) {
+    }
+    
+    @IBAction func intensityChanged(_ sender: Any) {
+        applyProcessing()
+    }
+    
+    func applyProcessing(){
+        ///read output image from current filter
+        guard let image = currentFilter.outputImage else {return}
+        ///use slider value to set kCIInputIntensityKey
+        currentFilter.setValue(intensity.value, forKey: kCIInputIntensityKey)
+        ///creates a new data type called CGIImage from output image of current filter
+        if let cgimg = context.createCGImage(image, from: image.extent) {
+            ///create UIImage from CGIImage
+            let processedImage = UIImage(cgImage: cgimg)
+            imageView.image = processedImage
+        }
     }
     
 }
